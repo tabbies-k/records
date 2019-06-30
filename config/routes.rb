d0d9ec928f0024d848a670a7d3015b8accf47bbc
tabbies <botantotorao@gmail.com>
2019-06-30 02:17:36 +0000

    Add user index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   devise_for :users
   resources :records
+  get 'users' => "users#index"
+  get 'users/:id' => "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
